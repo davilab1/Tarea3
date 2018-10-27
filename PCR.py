bd36fb54292bfef5ee4861ca62d5f75f71a16b7c
Dario Avila <DarioAvila@Darios-MacBook-Air.local>
2018-10-27 15:40:37 -0500

    Avance y subiendo .dat

--- PCR.py
+++ PCR.py
@@ -3,14 +3,15 @@ import matplotlib.pylab as plt
 import math
 
 #Almacene los datos del archivo WDBC.dat.
-datos=np.getfromtxt("WDBC.dat",delimiter=",")
+datos=np.genfromtxt("WDBC.dat",delimiter=",")
 columna=datos[0]
 fila=datos[1,:]
 
 # Calcule, con su implementacion propia, la matriz de covarianza de los datos y la imprima
-def pca(dat,n):
 
-    mat=np.zeros(len(filas),len(columnas))
+def pca(dat,n,filas,columnas):
+
+    mat=np.zeros((len(filas),len(columnas)))
 
     for i in range(n,len(fila)):
         for j in range(n,len(fila)):
@@ -20,9 +21,11 @@ def pca(dat,n):
                 xsuma=x1*x2
                 k+=xsuma
             mat[i,j]=xsuma/(len(columna)-1)
-
+    return mat
+matrizcov=pca(datos,2,fila,columna)
+print(matrizcov)
 #Calcule los autovalores y autovectores de la matriz de covarianza y los imprima (para esto puede usar los paquetes de linalg de numpy). Su mensaje debe indicar explıcitamente cual es cada autovector y su autovalor correspondiente.
-resolviendo=np.linalg.eig(mat)
+resolviendo=np.linalg.eig(matrizcov)
 
 eigvalues=resolviendo[0]
 print("Los valores propios de la matriz de covarianza son",eigvalues)
@@ -33,7 +36,13 @@ print("Los vectores propios correspondientes de la matriz de covarianza son",eig
 print("Teniendo en cuenta los autovectores, los parametros mas importantes son")
 
 #Haga una proyeccion de sus datos en el sistema de coordenadas PC1, PC2 y grafique estos datos. Use un color distinto para el diagnostico maligno y el benigno y la guarde dicha grafica sin mostrarla en ApellidoNombre_PCA.pdf.
-
+'''plt.figure()
+plt.plot(label="Maligno",color="red")
+plt.plot(label="Benigno",color="blue")
+plt.title('Proyeccion de Datos en sistema de coordenadas PCA')
+plt.xlabel()
+plt.ylabel()
+plt.savefig("")'''
 
 #Imprima un mensaje diciendo si el metodo de PCA es util para hacer esta clasificacion, si no sirve o si puede ayudar al diagnostico para ciertos pacientes, argumentando claramente su posicion
- print("El metodo de PCa resulta ...para hacer esta clasificacion")
+print("El metodo de PCa resulta ...para hacer esta clasificacion")
